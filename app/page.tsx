64932e70c4c8eff3ed039ea9f8e594d8be439f08
Steve Kim <Stevekim0315@gmail.com>
2023-12-05 20:30:12 +1100

    30 commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 // Import necessary libraries/components
 import Image from 'next/image';
+import profilePic from '../public/logo-icon.png'
 
 const HomePage = () => {
   return (
     <div className="w-full flex flex-col items-center mt-24">
-      <Image src="/logo-icon.png" alt="Your Logo Alt Text" width={500} height={300} />
+      <Image
+      src={profilePic}
+      alt="Picture of the author"
+      width={500} 
+      height={500} 
+      blurDataURL="data:..."
+      placeholder="blur" 
+    />
     </div>
   );
 };
