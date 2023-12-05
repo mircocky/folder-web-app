18800744afcf4eac4654190fbe1a939d4e0d0829
Steve Kim <Stevekim0315@gmail.com>
2023-12-05 19:52:23 +1100

    22 commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const HomePage = () => {
 
 return (
     <div className="w-full flex flex-col items-center mt-24">
-      <Image src="/Logo.png" alt="Logo" width={500} height={300}/>
+      <Image src="/logo.png" alt="logo" width={500} height={300}/>
     </div>
   );
 };
